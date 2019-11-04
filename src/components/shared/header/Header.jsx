6e119ad6fa9bd9a5409815a594a88e658cf924c1
Søren Gravesen <S_gravesen92@hotmail.com>
2019-11-04 22:04:19 +0100

    (grav) add login modal

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,14 +1,44 @@
 import React, { Component } from "react";
+import Modal from "./LoginModal";
 import { Wrapper, Logo, LoginSignupButton, Container } from "./styles";
+import LoginModal from "./LoginModal";
 
 export default class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            modalOpen: true
+        };
+
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    openModal() {
+        this.setState({
+            modalOpen: true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            modalOpen: false
+        });
+    }
+
     render() {
+        const { modalOpen } = this.state;
+
         return (
             <Wrapper>
                 <Container>
                     <Logo>Online Personal Diary</Logo>
-                    <LoginSignupButton>Login</LoginSignupButton>
+                    <LoginSignupButton onClick={this.openModal}>
+                        Log in
+                    </LoginSignupButton>
                 </Container>
+                <LoginModal active={modalOpen} closeModal={this.closeModal} />
             </Wrapper>
         );
     }
--- src/components/shared/header/styles.js
+++ src/components/shared/header/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { breakpoints } from "../../../frontend-config";
 
+// Header
 export const Wrapper = styled.div`
     height: 80px;
     background: ${props => props.theme.primary.darkblue};
@@ -46,5 +47,70 @@ export const LoginSignupButton = styled.button`
     &:active {
         transform: translateY(calc(-50% + 1px));
         box-shadow: 1px 2px 0 0 rgba(0, 0, 0, 0.2);
+    }
+`;
+
+// Modal
+export const ModalContainer = styled.div`
+    transition: all ease 0.5s;
+    ${props =>
+        props.active
+            ? `opacity: 1;
+        visibility: visible;`
+            : `
+        opacity: 0;
+        visibility: hidden;
+    `}
+`;
+
+export const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.4);
+`;
+
+export const ModalWrapper = styled.div`
+    position: fixed;
+    transform: translate(50%, calc(-50% + 40px));
+    top: 50%;
+    right: 50%;
+    width: 90vw;
+    max-height: calc(80vh - 80px);
+    background: #fff;
+    max-width: 400px;
+    padding: 20px 0;
+`;
+
+export const ModalHeadline = styled.div`
+    text-align: center;
+    font-size: 40px;
+    font-weight: bold;
+    padding-bottom: 10px;
+`;
+
+export const ModalForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const ModalInput = styled.input`
+    margin-bottom: 15px;
+    padding: 3px 5px;
+    width: 230px;
+`;
+
+export const ModalLoginButton = styled(LoginSignupButton)`
+    position: relative;
+    background: ${props => props.theme.primary.darkblue};
+
+    &,
+    &:hover,
+    &:active {
+        transform: translate(0);
+        box-shadow: none;
     }
 `;
